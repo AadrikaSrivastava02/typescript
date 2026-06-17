d68ff62e222243d66f528c8bcd68405272c04bb8
Aadrika Srivastava <121657643+AadrikaSrivastava02@users.noreply.github.com>
2026-06-18 01:35:39 +0530

    first commit

---
+++ dist/indext.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let username = "Sarthak";
+let age = 25;
+console.log(`Name: ${username}`);
+console.log(`Age: ${age}`);
+//# sourceMappingURL=indext.js.map
---
+++ src/indext.ts
@@ -0,0 +1,5 @@
+let username: string = "Sarthak";
+let age: number = 25;
+
+console.log(`Name: ${username}`);
+console.log(`Age: ${age}`);
