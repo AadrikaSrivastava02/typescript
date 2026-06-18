f9d6fad577a9f5ea54e5b8e4b2f59caf167a3db5
Aadrika Srivastava <121657643+AadrikaSrivastava02@users.noreply.github.com>
2026-06-18 17:08:21 +0530

    day2a

--- src/indext.ts
+++ src/indext.ts
@@ -1,5 +1,8 @@
 let username: string = "aadrika";
 let age: number = 100;
+let class: string = "10th"; 
+
 
 console.log(`Name: ${username}`);
 console.log(`Age: ${age}`);
+console.log(`Class: ${class}`);
